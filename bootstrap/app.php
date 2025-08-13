14b31ccdd416904aee6c4693309c848bf4be96a6
rushit patel <rushitpatel304@gmail.com>
2025-08-13 18:37:39 +0530

    add privacy policy page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,6 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'team/task/api/temp/store-file',
             'webhook/*',
+            'webhook',
             'facebook/webhook'
         ]);
     })
